c001d5cca497c5462d6821b62ce0fd0b9018c53b
Ben Hentschel <124124446+Bheny07@users.noreply.github.com>
2024-03-27 14:07:54 +1300

    Welcoming v2

--- welcoming_v2.py
+++ welcoming_v2.py
@@ -1,4 +1,8 @@
-"""Adding on from Version 1 but giving the user options on where to go next"""
+"""Adding on from Version 1 but giving the user options on where to go next,
+and also adding it to a loop."""
+
 import easygui
-easygui.buttonbox("Welcome to Bens Burgers\n How Can I Help You?",
-                  choices=["Menu", "Order", "Exit", "Help"])
+
+while True:
+    easygui.buttonbox("Welcome to Bens Burgers\n How Can I Help You?",
+                      choices=["Menu", "Order", "Exit", "Help", "Edit Menu"])
