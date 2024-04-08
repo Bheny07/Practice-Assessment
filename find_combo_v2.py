7163fac5e469e7a1ae1890b495576aebf2cc63c8
Ben Hentschel <124124446+Bheny07@users.noreply.github.com>
2024-04-09 10:07:09 +1200

    commit

---
+++ find_combo_v2.py
@@ -0,0 +1,48 @@
+"""Adding on from v1 but using easygui to make the interface more aesthetic and
+easy to use for the user"""
+import easygui
+
+combo = {
+    "Value":
+        {"Item 1": {"Name": "Beefburger", "Price": 5.69},
+         "Item 2": {"Name": "Fries", "Price": 1.00},
+         "Item 3": {"Name": "Fizzy Drink", "Price": 1.00},
+         "Total": "$7.69"
+         },
+    "Cheezy":
+        {"Item 1": {"Name": "Cheeseburger", "Price": 6.69},
+         "Item 2": {"Name": "Fries", "Price": 1.00},
+         "Item 3": {"Name": "Fizzy Drink", "Price": 1.00},
+         "Total": "$8.69"
+         },
+    "Super":
+        {"Item 1": {"Name": "Cheeseburger", "Price": 6.69},
+         "Item 2": {"Name": "Large Fries", "Price": 2.00},
+         "Item 3": {"Name": "Smoothie", "Price": 2.00},
+         "Total": "$10.69"
+         },
+}
+
+
+def search_combo():
+    combo_search = easygui.enterbox("Enter the name of the combo you "
+                                    "want to search for: ",
+                                    title="Combo Search")
+
+    found = False
+    for combo_name, combo_info in combo.items():
+        if combo_search.lower() == combo_name.lower():
+            found = True
+            message = f"\nCombo Name: {combo_name}\n"
+            for key, value in combo_info.items():
+                if key != "Total":
+                    message += f"{key}: {value['Name']} - ${value['Price']}\n"
+                else:
+                    message += f"{key}: {value}\n"
+            easygui.msgbox(message, title="Menu Combo")
+            break
+    if not found:
+        easygui.msgbox("Combo not found in the Menu.", title="Error")
+
+
+search_combo()
--- main_code_v4.py
+++ main_code_v4.py
@@ -1,3 +1,6 @@
+"""Adding find_combo v2 into the main code and making slight
+adjustments to the code so, it all works and functions together as it should"""
+
 import easygui
 
 combo = {
@@ -22,22 +25,73 @@ combo = {
 }
 
 
+def search_combo(combos):
+    combo_search = easygui.enterbox("Enter the name of the combo you "
+                                    "want to search for: ",
+                                    title="Combo Search")
+    found = False
+    for combo_name, combo_info in combos.items():
+        if combo_search.lower() == combo_name.lower():
+            found = True
+            message = f"\nCombo Name: {combo_name}\n"
+            for key, value in combo_info.items():
+                if key != "Total":
+                    message += f"{key}: {value['Name']} - ${value['Price']}\n"
+                else:
+                    message += f"{key}: {value}\n"
+            easygui.msgbox(message, title="Menu Combo")
+            break
+    if not found:
+        easygui.msgbox("Combo not found in the Menu.", title="Error")
+
+
 def display_combo_info(combos):
     message = "Combo Information:\n\n"
     for combo_id, combo_info in combos.items():
         message += f"Combo Name: {combo_id}\n"
         message += "-" * 50 + "\n"
+        total_price = 0
         for key, item_info in combo_info.items():
-            if key != "Total":
+            if key != "Total" and isinstance(item_info, dict):
                 price = float(item_info['Price'])
-                message += f"* {key}: {item_info['Name']} " \
-                           f"(${price:.2f})\n"
-        if combo_info['Total'] is not None:
-            total_prices = float(combo_info['Total'][1:])
-            message += f"Total Price: ${total_prices:.2f}\n"
-        message += "\n"
+                total_price += price
+                message += f"* {key}: {item_info['Name']} (${price:.2f})\n"
+        message += f"Total Price: ${total_price:.2f}\n\n"
 
     easygui.msgbox(message, title="Menu", ok_button="Close")
+
+
+def get_valid_price_input(price):
+    while True:
+        try:
+            price = float(easygui.enterbox(price))
+            if price < 0:
+                easygui.msgbox("Price cannot be negative. "
+                               "Please enter a valid number.")
+            else:
+                return round(price, 2)
+        except ValueError:
+            easygui.msgbox("Invalid input. Please enter a valid number.")
+
+
+def add_combo_item():
+    combo_ID = easygui.enterbox("\nEnter Combo: ")
+    combo[combo_ID] = {}
+
+    Item_1 = easygui.enterbox("Enter Item 1: ")
+    combo[combo_ID]['Item 1'] = {"Name": Item_1}
+    Item_1_Price = get_valid_price_input("Enter Item 1 Price: ")
+    combo[combo_ID]['Item 1']['Price'] = Item_1_Price
+
+    Item_2 = easygui.enterbox("Enter Item 2: ")
+    combo[combo_ID]['Item 2'] = {"Name": Item_2}
+    Item_2_Price = get_valid_price_input("Enter Item 2 Price: ")
+    combo[combo_ID]['Item 2']['Price'] = Item_2_Price
+
+    Item_3 = easygui.enterbox("Enter Item 3: ")
+    combo[combo_ID]['Item 3'] = {"Name": Item_3}
+    Item_3_Price = get_valid_price_input("Enter Item 3 Price: ")
+    combo[combo_ID]['Item 3']['Price'] = Item_3_Price
 
 
 while True:
@@ -48,9 +102,9 @@ while True:
     if choice == "Menu":
         display_combo_info(combo)
     elif choice == "Add Combo":
-        pass
+        add_combo_item()
     elif choice == "Find Combo":
-        pass
+        search_combo(combo)
     elif choice == "Delete Combo":
         pass
     elif choice == "Exit":
